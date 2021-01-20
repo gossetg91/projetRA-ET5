bcce87a7e7baf0bcb565e431a9c9a86eb08f189a
Guillaume Gosset <gossetg91@gmail.com>
2021-01-20 12:33:07 +0100

    basic setup for debug

---
+++ Assets/Resources/Scripts/objectReplacement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class objectReplacement : MonoBehaviour
+{
+    public GameObject boardTag;
+    public GameObject referenceTag;
+    public Text toModify;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        toModify.text = boardTag.GetComponent<Transform>().position.ToString();
+    }
+}
