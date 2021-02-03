525eb5198d6b24d279893ea4032c869a2a9bfbbe
Arugakente <arugakente@gmail.com>
2021-02-03 14:04:54 +0100

    Money menu WIP

---
+++ Assets/Resources/Script/FadeScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeScript : MonoBehaviour
+{
+    public double timeCounter = 0.2;
+    //double alphaInit = GetComponent<Image>().color.a;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Fade()
+    {
+        for (double i=0.0; i<timeCounter; i+=Time.deltaTime)
+        {   
+            //GetComponent<Image>().color.a = i/timeCounter * alphaInit;
+        }
+    }
+}
---
+++ Assets/Resources/Script/UIScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIScript : MonoBehaviour
+{
+    public Canvas moneyMenu;
+
+    public Text moneyText;
+    int money = 2000;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ToggleMoneyMenu();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    public void AddMoney(int amount)
+    {
+        money += amount;
+    }
+
+    public void RemoveMoney(int amount)
+    {
+        money -= amount;
+    }
+
+    public void Add200()
+    {
+        AddMoney(200);
+    }
+
+
+
+    public void UpdateMoney()
+    {
+        moneyText.text = money.ToString() + " $";
+    }
+
+    public void ToggleMoneyMenu() 
+    {
+        moneyMenu.gameObject.SetActive(!moneyMenu.gameObject.activeSelf);
+    }
+    
+}
