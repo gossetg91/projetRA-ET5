ba1771d719fe6e67c8583a3dfaa5b99506156889
Guillaume Gosset <gossetg91@gmail.com>
2021-02-10 12:34:47 +0100

    Auto token displacement implementation

--- Assets/Resources/Scripts/objectReplacement.cs
+++ Assets/Resources/Scripts/objectReplacement.cs
@@ -7,17 +7,30 @@ public class objectReplacement : MonoBehaviour
 {
     public GameObject boardTag;
     public GameObject referenceTag;
-    public Text toModify;
+    public GameObject token;
 
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        toModify.text = (boardTag.GetComponent<Transform>().position-referenceTag.GetComponent<Transform>().position).ToString();
+        token.GetComponent<Transform>().position = referenceTag.GetComponent<Transform>().position;
+        token.GetComponent<Transform>().position += new Vector3(0.0f,0.015f,0.0f);
+        Vector3 xAxis = new Vector3(1.0f,0.0f,0.0f);
+        Vector3 zAxis = new Vector3(0.0f,0.0f,1.0f);
+        Vector3 differenceBoardTag = boardTag.GetComponent<Transform>().position-referenceTag.GetComponent<Transform>().position;
+        differenceBoardTag.y = 0;
+        float offsetFactor = 1-Mathf.Abs(((Mathf.Abs(Vector3.Dot(Vector3.Normalize(differenceBoardTag),xAxis))-Mathf.Abs(Vector3.Dot(Vector3.Normalize(differenceBoardTag),zAxis)))-0.1f));
+        offsetFactor = differenceBoardTag.magnitude-(0.10f+0.05f*offsetFactor);
+        differenceBoardTag= Vector3.Normalize(differenceBoardTag);
+        differenceBoardTag *= offsetFactor;
+
+        if(offsetFactor>0)
+        {
+            token.GetComponent<Transform>().position += differenceBoardTag;
+        }
     }
 }
